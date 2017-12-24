3b8908a471f6ebcdb83e91065c38b69c235e82bf
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2017-12-24 14:23:56 +0100

    portus

--- templates/portus/0/start.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p {certs,data}
-sudo certbot certonly --agree-tos --standalone -d portus.staging.teco.edu --email admin@staging.teco.edu
-cp /etc/letsencrypt/live/portus.staging.teco.edu/* certs/
