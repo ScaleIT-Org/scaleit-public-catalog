bd4057b77a47f8b6e965b8aac9ce38be8542826b
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-02-13 09:37:05 +0100

    script update

--- autoload.sh
+++ autoload.sh
@@ -1,19 +1,38 @@
-#!/bin/bash 
-echo Creating new version ${1##*/} App in catalog
-cd ${1##*/}
+#!/bin/bash
 
+name=${1##*/}
 
+if [ ! -d "templates/$name" ]; then
+  echo App $name does not exist
+  echo Creating $name template
+  mkdir templates/$name
+fi
+
+# Go to folder with the App.
+echo Creating new version $name App in catalog
+cd templates/$name
+
+# Cloning the repository with the App.
 echo Cloning $1
 git clone $1
-cd ${1##*/}
+cd $name
 
-log=$(git log -1 --pretty --oneline)
-log=${log##* }
-cd ..
-echo App version $log
+# Copy the icon
+mv $name/catalogIcon-$name.*  .
 
+if [ ! -f "config.yml" ]; then
+  echo Creating config.yml
+  echo Write description of the App
+  read $description
+  echo "name: $name
+  description: |
+    $description
+  category: Applications" >> config.yml
+fi
+
+# Count the folders which names are version numbers and set the new version
+# number.
 version=0
-
 for f in  ./* ; do
   if [ -d $f ]
     then
@@ -25,11 +44,30 @@ for f in  ./* ; do
 done;
 echo Number $version
 
-cp -r ./$((version-1)) ./$version
+# Creating rancher.compose.yml
+echo "version: '2'
+catalog:
+  name: \"$name\"
+  version: \"$version\"" >> rancher-compose.yml
+# Read environments variable from .env.default and parse them to questions in
+# rancher-compose
+if [ -f ".env.default" ]; then
+  echo "  questions:" >> rancher-compose.yml
+  cat .env | while read line; do
+    echo $line
+    echo "    - variable: \"${line%=*}\"" >> rancher-compose.yml
+    echo "      default: ${line##*=}" >> rancher-compose.yml
+  done
+fi
 
-mv ./${1##*/}/docker-compose.yml ./$version/docker-compose.yml
-rm -rf ./${1##*/}
+# Move the docker-compose.yml from cloned repo to the new version folder.
+mv ./$name/docker-compose.yml ./$version/docker-compose.yml
 
+# Remove the cloned repo.
+rm -rf ./$name
+
+# Change version of docker-compose file to 2 and change App version name in
+# rancher-compose.
 cd ./$version
 sed -i '' 's/version: .*/version: "2"/g'  docker-compose.yml
-sed -i '' 's/ version: .*/ version: "'$log'"/g'  rancher-compose.yml
+sed -i '' 's/ version: .*/ version: "'$version'"/g'  rancher-compose.yml
--- test.sh
+++ test.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 # ./autoload.sh ${1}
-name=templates/${1##*/}
-cd name
+name=${1##*/}
+cd templates/name
+
 # Validate the configuration of docker-compose.yml file.
-DCONFIG=$(docker-compose -f docker-compose.yml config -q)
+DCONFIG=$(docker-compose config -q)
 if [ -z $DCONFIG ]
 then
 	echo Docker compose syntax ... OK
 else echo Docker compose syntax ... ERROR: NOT CORRECT
 fi
+
 # Test if an image exists.
-if [ ! $(ls -1A catalogIcon-${1##*/}.* 2>/dev/null | wc -l) -gt 0 ] then
-    echo Icon file for app store ... ERROR: NOT FOUND
+if [ ! $(ls -1A catalogIcon-$name.* 2>/dev/null | wc -l) -gt 0 ] then
+    echo Icon file for app store ... WARNING: NOT FOUND
 else echo Icon file for app store ... OK
 fi
