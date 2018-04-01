2d8adb0657d323946ed3390493b0a50d5e2bf8e1
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-04-01 13:12:24 +0200

    script update + proxy eintrag

--- autoload.sh
+++ autoload.sh
@@ -22,7 +22,7 @@ mv $name/catalogIcon-$name.*  .
 if [ ! -f "config.yml" ]; then
   echo Creating config.yml
   echo Write description of the App
-  read $description
+  read description
   echo "name: $name
   description: |
     $description
@@ -51,18 +51,21 @@ catalog:
 
 # Read environments variable from .env.default and parse them to questions in
 # rancher-compose
+cd $name
 if [ -f ".env.default" ]; then
-  echo "  questions:" >> ./$version/rancher-compose.yml
+  echo "  questions:" >> rancher-compose.yml
   cat .env | while read line; do
     echo $line
-    echo "    - variable: \"${line%=*}\"" >> ./$version/rancher-compose.yml
-    echo "      default: ${line##*=}" >> ./$version/rancher-compose.yml
+    echo "    - variable: \"${line%=*}\"" >> rancher-compose.yml
+    echo "      default: ${line##*=}" >> rancher-compose.yml
   done
 fi
+cd ..
 
-# Move the docker-compose.yml from cloned repo to the new version folder.
+# Move the docker-compose.yml and rancher-compose.yml from cloned repo to the
+# new version folder.
 mv ./$name/docker-compose.yml ./$version/docker-compose.yml
-
+mv ./$name/rancher-compose.yml ./$version/rancher-compose.yml
 # Remove the cloned repo.
 rm -rf ./$name
 
